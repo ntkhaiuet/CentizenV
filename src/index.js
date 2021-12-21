8b7bd49299fd6be8025bfb3a9f5f3d7f882e0f3d
ntkhaiuet <ntkhaiuet@gmail.com>
2021-12-21 15:03:18 +0700

    router & controller

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const { extname, dirname } = require("path");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 // Folder static
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,19 +25,8 @@ app.engine("hbs", engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  console.log(req.body);
-  res.send("");
-});
-
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
