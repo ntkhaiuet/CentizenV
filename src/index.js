b34908faa3b76ead7fb0d522ca655461354bfcc3
ntkhaiuet <ntkhaiuet@gmail.com>
2021-12-21 13:01:24 +0700

    bootstrap 4

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const { extname, dirname } = require("path");
 const app = express();
 const port = 3000;
 
+// Folder static
 app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
